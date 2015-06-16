6a5d1ec0e70ec3606f09841475b050d7ddc4f6c8
Boris <b8427@yandex.ru>
2015-06-16 21:25:14 +0300

    class Example2

---
+++ src/Chapter02/Example2.java
@@ -0,0 +1,15 @@
+package Chapter02;
+
+/**
+ * Created by Boris on 16.06.2015.
+ */
+public class Example2 {
+    public static void main(String[] args) {
+        int num;
+        num=100;
+        System.out.println("Это переменная num: "+num);
+        num=num*2;
+        System.out.print("Значение переменной num * 2 равно ");
+        System.out.println(num);
+    }
+}
