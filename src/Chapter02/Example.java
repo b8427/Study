533f85b09e7e21c44bd55e89396fd18c4a557a41
Boris <b8427@yandex.ru>
2015-06-16 20:59:08 +0300

    class Examle

---
+++ src/Chapter02/Example.java
@@ -0,0 +1,8 @@
+package Chapter02;
+
+public class Example {
+
+    public static void main(String args[]) {
+        System.out.println("Это простая программа на Java.");
+    }
+}
--- src/Chapter02/Joppa.java
+++
@@ -1,8 +0,0 @@
-package Chapter02;
-
-public class Joppa {
-
-    public static void main(String[] args) {
-        System.out.println("Joppa");
-    }
-}
