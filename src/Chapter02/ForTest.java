3b92884585410a086ee3d16f7275841fd4bdc2b0
Boris <b8427@yandex.ru>
2015-06-18 20:00:39 +0300

    class ForTest

---
+++ src/Chapter02/ForTest.java
@@ -0,0 +1,14 @@
+package Chapter02;
+
+/**
+ * Created by Boris on 18.06.2015.
+ */
+public class ForTest {
+    public static void main(String[] args) {
+        int x;
+        for(x=0;x<10;x++){
+            System.out.println("Значение х:"+x);
+        }
+    }
+
+}
