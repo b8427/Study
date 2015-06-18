ef4f5d0f946b6364e85b8fd7ad072a6d750c7c3e
Boris <b8427@yandex.ru>
2015-06-18 21:06:23 +0300

    class Area

---
+++ src/Chapter02/Area.java
@@ -0,0 +1,17 @@
+package Chapter02;
+
+/**
+ * Created by Boris on 18.06.2015.
+ */
+public class Area {
+    public static void main(String[] args) {
+        double pi;
+        double area;
+        double r;
+
+        r=10.8;
+        pi=3.141592654;
+        area=pi*Math.pow(r,2);
+        System.out.println("Площадь круга равна "+area);
+    }
+}
