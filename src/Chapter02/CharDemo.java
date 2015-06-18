58251ee6bd17019c788e4079f870c78774a8903a
Boris <b8427@yandex.ru>
2015-06-18 21:15:10 +0300

    class CharDemo

---
+++ src/Chapter02/CharDemo.java
@@ -0,0 +1,17 @@
+package Chapter02;
+
+/**
+ * Created by Boris on 18.06.2015.
+ */
+public class CharDemo {
+    public static void main(String[] args) {
+        char ch1;
+        char ch2;
+
+        ch1=88;
+        ch2='Y';
+
+        System.out.println(ch1+" "+ch2);
+
+    }
+}
