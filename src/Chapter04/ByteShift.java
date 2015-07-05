3b317224559443a79584a365c8b161c8f71a2de5
Boris <b8427@yandex.ru>
2015-07-05 20:14:13 +0300

    refresh

--- src/Chapter04/ByteShift.java
+++ src/Chapter04/ByteShift.java
@@ -5,13 +5,23 @@ package Chapter04;
  */
 public class ByteShift {
     public static void main(String[] args) {
-        byte a = 64;
-        byte b;
+       // byte a = 64;
+     //   byte b;
         int i;
-        i = a << 2;
-        b = (byte) (a << 2);
-        System.out.println("Пepвoнaчaльнoe  значение  а:  " + a);
-        System.out.println("i  and  b:  " + i + "  " + b);
+     //   i = a << 2;
+     //   b = (byte) (a << 2);
+      //  System.out.println("Пepвoнaчaльнoe  значение  а:  " + a);
+      //  System.out.println("i  and  b:  " + i + "  " + b);
+       double x1 = 14.0;
+        double x2 = 9.10;
+        double x3 = x1-x2;
+        System.out.println(x3);
+
+        int a,b,c;
+        a=24; b=60;c=1000;
+        long micros=a*b*b*c*c;
+        long millis = a*b*b*c;
+        System.out.println(micros/millis);
 
     }
 
